b24c6e0a5d17f5ef53b9db5bb5bae68b8748b816
Uzair Ansari <uzair@ubuntu1.localdomain>
2024-07-31 16:29:23 +0530

    Removed extra files

---
+++ Labfiles/01-prebuild-models/Python/invoice-analysis.py
@@ -0,0 +1,57 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+# Store connection information
+endpoint = "https://doc-intel-read2.cognitiveservices.azure.com/"
+key = "36ec0703357e4866a1bd04978304e806"
+
+fileUri = "https://github.com/uzi4/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true"
+fileLocale = "en-US"
+fileModelId = "prebuilt-invoice"
+
+print(f"\nConnecting to Forms Recognizer at: {endpoint}")
+print(f"Analyzing invoice at: {fileUri}")
+
+
+
+# Create the client
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+
+# Analyze the invoice
+poller = document_analysis_client.begin_analyze_document_from_url(
+    fileModelId, fileUri, locale=fileLocale
+)
+
+
+# Display invoice information to the user
+receipts = poller.result()
+
+for idx, receipt in enumerate(receipts.documents):
+    
+    vendor_name = receipt.fields.get("VendorName")
+    if vendor_name:
+        print(f"\nVendor Name: {vendor_name.value}, with confidence {vendor_name.confidence}.")
+
+
+    customer_name = receipt.fields.get("CustomerName")
+    if customer_name:
+        print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
+
+
+    invoice_total = receipt.fields.get("InvoiceTotal")
+    if invoice_total:
+        print(f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence {invoice_total.confidence}.")
+
+print("\nAnalysis complete.\n")
+
+
+# List all fields
+keys = list(receipt.fields.keys())
+
+
+# List all fields with values
+for key, value in receipt.fields.items():
+    print(f"{key} : {value.content}\n")
